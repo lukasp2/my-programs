9da606ee4b059e42350440ffea86297748fb55a7
Lukas Pohlman <lukas@desktop>
2020-10-05 22:10:46 +0200

    started termux script

---
+++ termux/control.py
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+## prerequisites ##
+# set target username, host and display
+target_usr="lukas"
+target_ip="192.168.2.175"
+target_display=":1" # check with $who ?
+
+# set up ssh keys if not already done
+#ssh-keygen
+#ssh-copy-id ${target_usr}@${target_ip}
+
+source resources.sh
+
+display_links() {
+    #employ python script with bea soup
+}
+
+ssh ${target_usr}@${target_ip}
+firefox-esr --display=${target_display}
+
+while [ 1 ]; do
+    echo "where you wanna go?"
+    read input
+    
+    case  $input  in
+        "yt")
+	    link="www.youtube.com"
+	    firefox-esr -new-tab=${link}
+	    display_links ${link}
+            ;;
+        "nf")       
+	    link="www.nyafilmer.wtf"
+	    firefox-esr -new-tab=${link}
+	    display_links ${link}
+            ;;
+        *)              
+    esac 
+done
